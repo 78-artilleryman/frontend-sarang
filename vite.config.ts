5908b8a4806faddbd282e16de237256e99796d9c
78-artilleryman <ywy040150@gmail.com>
2025-04-06 03:14:53 +0000

    🔧 Chore: msw 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       strategies: "injectManifest",
       srcDir: "src",
       filename: "sw.ts",
       registerType: "autoUpdate",
       injectRegister: false,
-    
+
       manifest: {
         name: "sarang-mate",
         short_name: "sarang-mate",
